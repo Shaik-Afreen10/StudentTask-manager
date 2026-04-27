74d613afa280ab2cf0dd56d6cd77d407e4d703a9
Shaik Afreen10 <afshaik775@gmail.com>
2026-04-27 22:45:24 +0530

    Implemented authentication APIs (register & login)

--- server/index.js
+++ server/index.js
@@ -1,7 +1,50 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("DB Connected"))
+.catch(err => console.log(err));
+
+
+// ✅ ADD REGISTER API HERE
+app.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  await User.create({
+    email,
+    password: hashed
+  });
+
+  res.send("User registered");
+});
+
+
+// ✅ ADD LOGIN API BELOW IT
+app.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) return res.status(400).send("User not found");
+
+  const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+  if (!isMatch) return res.status(400).send("Wrong password");
+
+  const token = jwt.sign({ id: user._id }, "secretkey");
+
+  res.json({ token });
+});
+
 
 app.get("/", (req, res) => {
   res.send("API running");
